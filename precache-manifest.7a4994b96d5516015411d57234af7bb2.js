d21080e3f7f37947a424ad62faa837e3f2521d96
Quincy <kuncc@foxmail.com>
2021-03-31 22:50:10 +0800

    deploy

---
+++ precache-manifest.7a4994b96d5516015411d57234af7bb2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ce13ae9c424dd86c023a29a90ee142",
+    "url": "/xiyo-note-website/background.jpg"
+  },
+  {
+    "revision": "2bf6231bc72d01c71c4b",
+    "url": "/xiyo-note-website/css/app.4acad311.css"
+  },
+  {
+    "revision": "9b23371dab5c53a57aa1",
+    "url": "/xiyo-note-website/css/chunk-59868aec.c586c466.css"
+  },
+  {
+    "revision": "88f1391f17df575ebf68",
+    "url": "/xiyo-note-website/css/chunk-893e7754.fb3817a0.css"
+  },
+  {
+    "revision": "2d23d70a3342313b53c3",
+    "url": "/xiyo-note-website/css/chunk-e6706192.d430f228.css"
+  },
+  {
+    "revision": "333cfb0d176cfe3fc6b5",
+    "url": "/xiyo-note-website/css/chunk-eb366338.468fb6a3.css"
+  },
+  {
+    "revision": "074c0a1f25a7ec70b6ac",
+    "url": "/xiyo-note-website/css/chunk-vendors.00179a9d.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/xiyo-note-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/xiyo-note-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d8ce13ae9c424dd86c023a29a90ee142",
+    "url": "/xiyo-note-website/img/background.d8ce13ae.jpg"
+  },
+  {
+    "revision": "cf54c631650b828d9b53571182aef0c1",
+    "url": "/xiyo-note-website/index.html"
+  },
+  {
+    "revision": "2bf6231bc72d01c71c4b",
+    "url": "/xiyo-note-website/js/app.22bb19e3.js"
+  },
+  {
+    "revision": "98b5118322adbd9e7db2",
+    "url": "/xiyo-note-website/js/chunk-2d21af29.ccada908.js"
+  },
+  {
+    "revision": "7bc87ddb32bb19186658",
+    "url": "/xiyo-note-website/js/chunk-44c8b642.d45537d0.js"
+  },
+  {
+    "revision": "9b23371dab5c53a57aa1",
+    "url": "/xiyo-note-website/js/chunk-59868aec.66d887b9.js"
+  },
+  {
+    "revision": "88f1391f17df575ebf68",
+    "url": "/xiyo-note-website/js/chunk-893e7754.9e72d374.js"
+  },
+  {
+    "revision": "2d23d70a3342313b53c3",
+    "url": "/xiyo-note-website/js/chunk-e6706192.cc1268f9.js"
+  },
+  {
+    "revision": "333cfb0d176cfe3fc6b5",
+    "url": "/xiyo-note-website/js/chunk-eb366338.1d5df8bd.js"
+  },
+  {
+    "revision": "074c0a1f25a7ec70b6ac",
+    "url": "/xiyo-note-website/js/chunk-vendors.13aa9f8c.js"
+  },
+  {
+    "revision": "97328dbc304076195497ebfb3a181230",
+    "url": "/xiyo-note-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiyo-note-website/robots.txt"
+  }
+]);
